b8b8e17c8139fd41253eca46a551fd7bc130c190
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-08 17:16:04 +0200

    create view for show function

---
+++ resources/views/project.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="card border-dark mb-3">
+            <div class="card-header">{{$project->periodo}}</div>
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <h5 class="card-title">{{$project->nome}}</h5>
+                    <small class="card-title">{{$project->cliente}}</small>
+                </div>
+                <p class="card-text">{{$project->riassunto}}</p>
+            </div>
+        </div>
+    </div>
+@endsection
