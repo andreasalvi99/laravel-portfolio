95fc8ecfde9dc73ec533dd6bbd5ad02eb07e2888
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-08 17:19:50 +0200

    change card and button color

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="container mt-5">
         @foreach ($projects as $project )
-            <div class="card w-75 mb-3">
+            <div class="card border-dark mb-3">
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <h5 class="card-title">{{$project->nome}}</h5>
                         <p class="card-text">{{$project->periodo}}</p>
                     </div>
                     <p class="card-text">{{$project->cliente}}</p>
-                    <a href="{{ route('projects.show', $project)}}" class="btn btn-primary">More</a>
+                    <a href="{{ route('projects.show', $project)}}" class="btn btn-dark">More</a>
                 </div>
             </div>
         @endforeach
