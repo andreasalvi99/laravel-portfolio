3b518afda51bb6ee947ca6084f1aea76b642da51
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-11 19:54:47 +0200

    correct modal

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -23,11 +23,6 @@
                             <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 Elimina
                             </button>
-                            {{-- <form action="{{route('types.destroy', $type->id)}}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-outline-danger btn-sm">Elimina</button>
-                            </form> --}}
                         </div>
                     </div>
                 </div>
@@ -35,19 +30,24 @@
         @endforeach
     </div>
 
+    {{-- ^ Modale ^ --}}
+
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body fw-semibold fs-5">
                     Sei sicuro di voler eliminare l'elemento selezionato?
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Chiudi</button>
-                    <button type="button" class="btn btn-outline-danger btn-sm">Elimina</button>
+                    <form action="{{route('types.destroy', $type->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-outline-danger btn-sm">Elimina</button>
+                    </form>
                 </div>
             </div>
         </div>
