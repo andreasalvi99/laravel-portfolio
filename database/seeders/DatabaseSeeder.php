38feac8cf20685f5118f0f0402689fa93b51af17
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-11 17:45:27 +0200

    add seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([TypeTableSeeder::class,ProjectTableSeeder::class]);
+        $this->call([ProjectTableSeeder::class]);
     }
 }
