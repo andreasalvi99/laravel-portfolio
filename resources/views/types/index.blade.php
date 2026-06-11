507163231ed87b14ad6470006c0cc3bc2a44f6d8
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-11 19:08:27 +0200

    css adjustments

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -3,14 +3,26 @@
 @section('content')
     <div class="container mt-5">
         @foreach ($types as $type)
-            <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action">
+            <div class="card my-3">
+                <div class="card-header bg-white">
                     <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">{{$type->nome}}</h5>
                         <small class="text-body-secondary">3 days ago</small>
                     </div>
-                    <p class="mb-1">{{$type->descrizione}}</p>
-                </a>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <p class="mb-1 align-self-center">{{$type->descrizione}}</p>
+                        <div class="d-flex flex-column gap-1">
+                            <a href="#">
+                                <button class="btn btn-outline-warning btn-sm">Modifica</button>
+                            </a>
+                            <a href="#">
+                                <button class="btn btn-outline-danger btn-sm">Elimina</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         @endforeach
     </div>
