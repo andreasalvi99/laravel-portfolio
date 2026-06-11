f0be5e5fa55538b114ff7ab944b69069e6d2aea6
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-11 19:51:08 +0200

    add modale

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -20,15 +20,36 @@
                             <a href="{{route('types.edit', $type->id)}}">
                                 <button class="btn btn-outline-warning btn-sm">Modifica</button>
                             </a>
-                            <form action="{{route('types.destroy', $type->id)}}" method="POST">
+                            <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Elimina
+                            </button>
+                            {{-- <form action="{{route('types.destroy', $type->id)}}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <button class="btn btn-outline-danger btn-sm">Elimina</button>
-                            </form>
+                            </form> --}}
                         </div>
                     </div>
                 </div>
             </div>
         @endforeach
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Sei sicuro di voler eliminare l'elemento selezionato?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Chiudi</button>
+                    <button type="button" class="btn btn-outline-danger btn-sm">Elimina</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
