69c00a7672e6359b54b40ed62b217fc5d617bb8f
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-11 17:41:23 +0200

    done foreign key migration

---
+++ database/migrations/2026_06_11_151024_add_type_to_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('categoria');
+            $table->foreignId('type_id')->default(1)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('categoria');
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
+        });
+    }
+};
