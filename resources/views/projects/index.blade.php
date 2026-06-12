4363c8e927336090042b62fcdb322a56964f572d
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-12 10:58:18 +0200

    css adjustments

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -10,19 +10,21 @@
                 <div class="col">
                     <div class="card border-dark mb-3 h-100">
                         <div class="card-body">
-                            <div class="d-flex justify-content-between">
+                            <div class="d-flex justify-content-between mb-2">
                                 <h5 class="card-title">{{$project->nome}}</h5>
                                 <p class="card-text"><i>{{$project->periodo}}</i></p>
                             </div>
                             <div class="d-flex justify-content-between">
-                                <p class="card-text">{{$project->cliente}}
-                                    @if ($project->type)
-                                        <span class="d-block">{{$project->type->nome}}</span>    
-                                    @else
-                                        <span class="d-block">-</span>
-                                    @endif
-                                </p>
-                                <div class="d-flex flex-column gap-2">
+                                <div class="d-flex flex-column justify-content-center gap-2">
+                                    <p class="card-text m-0">{{$project->cliente}}</p>
+
+                                        @if ($project->type)
+                                            <p class="card-text m-0">{{$project->type->nome}}</p>    
+                                        @else
+                                            <p class="card-text m-0">-</p>
+                                        @endif
+                                </div>
+                                <div class="d-flex flex-column gap-2 justify-content-center">
                                     <a href="{{route('projects.edit', $project->id)}}">
                                         <button class="btn btn-outline-warning btn-sm">
                                             <i class="bi bi-pencil-fill"></i>
@@ -33,7 +35,7 @@
                                     </button>
                                 </div>
                             </div>
-                            <a href="{{ route('projects.show', $project)}}" class="btn btn-outline-dark">More</a>
+                            <a href="{{ route('projects.show', $project)}}" class="btn btn-outline-dark mt-3">More</a>
                         </div>
                     </div>
                 </div>
