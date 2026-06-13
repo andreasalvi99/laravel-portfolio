9e4ba1f0601ec3db6af74410c989168e08ac6c83
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-13 17:56:40 +0200

    delete method done

--- resources/views/technologies/index.blade.php
+++ resources/views/technologies/index.blade.php
@@ -11,11 +11,19 @@
                     <div class="card border mb-3 h-100">
                         <div class="card-header">
                             <small>
-                                <div class="d-flex justify-content-between">
+                                <div class="d-flex justify-content-between align-items-center">
                                     <span class="badge rounded-pill" style="background-color: {{$technology->colore}}">{{$technology->nome}}</span>
-                                    <a href="{{route('technologies.edit', $technology->id)}}">
-                                        <i class="bi bi-pencil-fill" style="color: {{$technology->colore}}"></i>
-                                    </a>
+                                    <button type="button" class="btn btn-outline dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                        <i class="bi bi-trash3-fill" style="color: {{$technology->colore}}"></i>
+                                    </button>
+
+                                    {{-- <form action="{{route('technologies.destroy', $technology->id)}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-outline-dark" type="submit">
+                                            <i class="bi bi-trash3-fill" style="color: {{$technology->colore}}"></i>
+                                        </button>
+                                    </form> --}}
                                 </div>
                                 
                             </small>
@@ -24,9 +32,41 @@
                             <h5 class="card-title">{{$technology->nome}}</h5>
                             <p class="card-text">{{$technology->descrizione}}</p>
                         </div>
+                        <div class="card-footer">
+                            <a href="{{route('technologies.edit', $technology->id)}}" class=" text-decoration-none text-dark">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-outline-dark">Modifica</button>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             @endforeach
+        </div>
+    </div>
+
+    {{-- ^ Modale ^ --}}
+
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body fs-4 fw-semibold">
+                    Sei sicuro di voler eliminare l'elemento selezionato?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Chiudi</button>
+                    <form action="{{route('technologies.destroy', $technology->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-outline-danger" type="submit">
+                           Elimina
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
