360683222ae207adcd4252566ef8f4ac726c82af
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-13 15:17:38 +0200

    rebuild database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([TechnologiesTableSeeder::class]);
+        $this->call([TechnologiesTableSeeder::class, TypeTableSeeder::class, ProjectTableSeeder::class]);
     }
 }
