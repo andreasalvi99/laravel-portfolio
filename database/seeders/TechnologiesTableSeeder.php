9bd8b250ad2e9163d175a9b1e3ada3bec90717ce
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-13 15:05:58 +0200

    seed done

--- database/seeders/TechnologiesTableSeeder.php
+++ database/seeders/TechnologiesTableSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TechnologiesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $technologies = ['HTML', 'CSS', 'React', 'Node.js', 'Express', 'JavaScript', 'Bootstrap', 'MySQL'];
+
+        foreach ($technologies as $technology) {
+            
+            $newTechnology = new Technology();
+
+            $newTechnology->nome = $technology;
+            $newTechnology->colore = $faker->hexColor();
+
+            $newTechnology->save();
+
+        }
     }
 }
